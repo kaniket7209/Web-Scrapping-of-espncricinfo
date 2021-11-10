7a4cd6c0e86814dbc4de650cefd953204c48df7b
kaniket7209 <72839172+kaniket7209@users.noreply.github.com>
2021-11-10 15:39:43 +0530

    Update birthdays.js

--- birthdays.js
+++ birthdays.js
@@ -1,68 +1,85 @@
-const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/chennai-super-kings-vs-kings-xi-punjab-53rd-match-1216506/full-scorecard";
-const request = require("request");
-const cheerio = require("cheerio");
+const request = require('request');
+const cheerio = require('cheerio');
+const { clear } = require('console');
+const chalk = require('chalk');
+// console.log("before");
+let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/chennai-super-kings-vs-kings-xi-punjab-53rd-match-1216506/full-scorecard";
+
 request(url, cb);
-function cb(err, response, html) {
-    if (err) {
-        console.log(err);
-    } else {
-        extractHtml(html);
+function cb(error, response, html) {
+    if (error) {
+        console.log("Page 404 not found");
+    }
+    else {
+        // console.log(html);
+        extracthmtl(html);
     }
 }
-function extractHtml(html) {
-    let $ = cheerio.load(html);
+// console.log("after");
+function extracthmtl(html) {
+    let $ = cheerio.load(html); // read the html for us
     let teamsArr = $(".match-info.match-info-MATCH .team");
-    let wteamname;
+    // winning team name
+    let wteamName;
     for (let i = 0; i < teamsArr.length; i++) {
         let hasclass = $(teamsArr[i]).hasClass("team-gray");
         if (hasclass == false) {
-            let teamnameele = $(teamsArr[i]).find(".name");
-            // console.log(teamnameele.text());
-            wteamname = teamnameele.text().trim();
-            // console.log(wteamname);
+            let teamNameEle = $(teamsArr[i]).find(".name");
+            wteamName = teamNameEle.text();
+            //  console.log(wteamName);
         }
     }
-    let innigsArr = $(".card.content-block.match-scorecard-table>.Collapsible");
 
-    for (let i = 0; i < innigsArr.length; i++) {
-        // let cHtml=$(innigsArr[i]).html();
-        // htmlstr+=cHtml;
-        let teamnameele = $(innigsArr[i]).find(".header-title.label");
-        let teamname = teamnameele.text();
-        teamname = teamname.split("INNINGS")[0];
-        teamname = teamname.trim();
-       
-       
-            // console.log(teamname);
-            let tableelem=$(innigsArr[i]).find(".table.batsman");
-            let allbowlers=$(tableelem).find("tr");
-            for(let j=0;j<allbowlers.length;j++){
-                let allcolsofplayers=$(allbowlers[j]).find("td");
-                let isbatsmancol=$(allcolsofplayers[0]).hasClass("batsman-cell");
-                if(isbatsmancol == true){
-                let href=$(allcolsofplayers[0]).find("a").attr("href");
-                let name=$(allcolsofplayers[0]).text();
-                // console.log(href);
-                let fulllink="https://www.espncricinfo.com"+href;
-                getbirthdaypage(fulllink,name,teamname);
-                // console.log(`teamname : ${teamname} playername: ${playername}`);
-                } 
+    // innings - 2 innings
+    let inningsArr = $(".card.content-block.match-scorecard-table .Collapsible");
+    for (let i = 0; i < inningsArr.length; i++) {
+        let teamNameEle = $(inningsArr[i]).find(".header-title.label");
+        let teamName = teamNameEle.text();
+        // TEAMNAME = Kings XI Punjab INNINGS (20 overs maximum) 
+        teamName = teamName.split("INNINGS")[0];
+        teamName = teamName.trim();
+        // comparing 
+        if (wteamName == teamName) {
+            let tableele = $(inningsArr[i]).find(".table.batsman tbody");
+            let allBatsmanEle = $(tableele).find("tr");
+            for (let j = 0; j < allBatsmanEle.length; j++) {
+                let allColsOfBatsman = $(allBatsmanEle[j]).find("td");
+                // console.log(allColsOfBatsman.text());
+                let isbatsmanpresent = $(allColsOfBatsman[0]).hasClass("batsman-cell");
+                if (isbatsmanpresent == true) {
+                    // find link 
+                    let href = $(allColsOfBatsman[0]).find("a").attr("href");
+                    let name = $(allColsOfBatsman[0]).text();
+                    // console.log(href);
+                    let fulllink = "https://www.espncricinfo.com" + href;
+                    // console.log(fulllink);
+                    getbirthdays(fulllink, name, teamName);
+                }
+            }
+
+        }
+
+    }
+}
+
+function getbirthdays(url, name, teamName) {
+    request(url, cb);
+    function cb(error, response, html) {
+        if (error) {
+            console.log("Page 404 not found");
+        }
+        else {
+            // console.log(html);
+            extractbirthdays(html, teamName, name);
         }
     }
-  }
-  function getbirthdaypage(url,name,teamname){
-      request(url,cb);
-      function cb(err,response,html){
-          if(err){
 
-          }else{
-              extractbirthday(html,name,teamname);
-          }
-      }
-  }
-  function extractbirthday(html,name,teamname){
-      let $= cheerio.load(html);
-      let detailsArr=$(".player-card-description");
-      let birthday=$(detailsArr[1]).text();
-      console.log(`${name}, plays for ${teamname} was born on ${birthday}`);
-  }
+}
+
+function extractbirthdays(html, teamName, name) {
+    let $ = cheerio.load(html);
+    let detailsArr = $(".player-card-description");
+    let birthday = $(detailsArr[1]).text();
+    // console.log(chalk.red(birthday));
+    console.log(`Batsman ${chalk.greenBright(name)} of team ${teamName} was born on ${birthday} `);
+}
